db7b48c5cf176e641fa2db73a8c7639aa055dafd
Ronaldo Barbachano <redcap3000@gmail.com>
2016-08-13 18:01:39 -0800

    code cleanup

--- imports/ui/Lane.jsx
+++ imports/ui/Lane.jsx
@@ -10,16 +10,12 @@ export default class Lane extends Component {
     super(props);
     Lanes.insert({title : ' '});
   }
-   dragOverLane(e){
+  dragOverLane(e){
     var lane = Lanes.findOne();
     if(this.props.title != lane.title){
       Contacts.update(lane.contact,{$set : { laneTitle : this.props.title} });
     }
 
-  }
-  drop(e){
-    e.preventDefault();
-    console.log('dropped');
   }
   // to do for highlighting of column
   isCurrentTitle(e){
@@ -30,7 +26,7 @@ export default class Lane extends Component {
   }
   render() {
     return (
-          <li className="lane" onDragOver={this.dragOverLane.bind(this)} onDrop={this.drop.bind(this)}>
+          <li className="lane" onDragOver={this.dragOverLane.bind(this)}>
             <h2>{this.props.title}</h2>
             <ul>{this.props.content}</ul>
           </li>
